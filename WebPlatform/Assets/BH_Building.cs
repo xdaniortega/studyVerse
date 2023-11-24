56c53f9aa2748119391c2d7f77631594f57eb5bc
Miquel Suau Gonzàlez <sg.miquel@gmail.com>
2023-11-24 21:12:01 +0100

    Loaded scene

---
+++ WebPlatform/Assets/BH_Building.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BH_Building : MonoBehaviour
+{
+    public GameObject mDestinationPoint;
+    public string mSceneToLoad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- WebPlatform/Assets/Scripts/BH_PlayerMovement.cs
+++ WebPlatform/Assets/Scripts/BH_PlayerMovement.cs
@@ -1,14 +1,21 @@
-using JetBrains.Annotations;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class BH_PlayerMovement : MonoBehaviour
 {
 	private NavMeshAgent navMeshAgent;
-	public readonly string CLICK_COMPARE_TAG = "Walkeable";
+	public readonly string CLICK_WALK_TAG = "Walkeable";
+	public readonly string CLICK_BUILDING_TAG = "InterBuilding";
+	public readonly string BUILDING_DEST_POINT = "BuildingDestPoint";
 	private bool mEnableMovement;
+
+	public delegate void PathCompleted(in string aSceneToLoad);
+	PathCompleted OnPathCompleted;
+	private string mSceneToLoad;
+
 	void Awake()
 	{
 		// Assuming your NavMeshAgent is attached to the same GameObject
@@ -31,19 +38,42 @@ public class BH_PlayerMovement : MonoBehaviour
 
 	void Update()
 	{
-		if (mEnableMovement && Input.GetMouseButtonDown(0))
+		if (mEnableMovement)
 		{
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit;
-
-			if (Physics.Raycast(ray, out hit))
+			if(Input.GetMouseButtonDown(0))
 			{
-				// Check if the clicked point is on the NavMesh
-				if (hit.collider.CompareTag(CLICK_COMPARE_TAG))
+				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				RaycastHit hit;
+				
+				if (Physics.Raycast(ray, out hit))
 				{
-					// Set the destination for the NavMeshAgent
-					navMeshAgent.SetDestination(hit.point);
+					// Check if the clicked point is on the NavMesh
+					if (hit.collider.CompareTag(CLICK_WALK_TAG))
+					{
+						// Set the destination for the NavMeshAgent
+						navMeshAgent.SetDestination(hit.point);
+					}
+					else if(hit.collider.CompareTag(CLICK_BUILDING_TAG))
+					{
+						BH_Building DestChildren = hit.transform.gameObject.GetComponent<BH_Building>();
+						if(DestChildren != null)
+						{
+							mSceneToLoad = DestChildren.mSceneToLoad;				
+							navMeshAgent.SetDestination(DestChildren.mDestinationPoint.transform.position);
+							OnPathCompleted = TestLog;
+						}
+						else
+						{
+							Debug.LogError("No destination children found.");
+						}
+					}
 				}
+			}
+
+			if(OnPathCompleted != null && navMeshAgent.hasPath && navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+			{
+				OnPathCompleted(mSceneToLoad);
+				OnPathCompleted = null;
 			}
 		}
 	}
@@ -59,5 +89,10 @@ public class BH_PlayerMovement : MonoBehaviour
 				mEnableMovement	= false;
 				break;
 		}
+	}
+
+	public void TestLog(in string aSceneToLoad)
+	{
+		SceneManager.LoadScene(aSceneToLoad, LoadSceneMode.Single);
 	}
 }
